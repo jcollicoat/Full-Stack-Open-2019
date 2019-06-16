25abcf21652ab343786a7133655a35f61facad6d
Joseph Collicoat <jcollicoat@gmail.com>
2019-06-16 17:34:09 +1200

    1.2 Commit
    course information, step2

--- part1/src/index.js
+++ part1/src/index.js
@@ -10,19 +10,52 @@ const Header = (props) => {
   )
 }
 
+const Part1 = () => {
+  const name = 'Fundamentals of React'
+  const exercises = 10
+
+  return (
+    <>
+      <p>
+        Name: {name} | No. of exercises: {exercises}
+      </p>
+    </>
+  )
+}
+
+const Part2 = () => {
+  const name = 'Using props to pass data'
+  const exercises = 7
+
+  return (
+    <>
+      <p>
+        Name: {name} | No. of exercises: {exercises}
+      </p>
+    </>
+  )
+}
+
+const Part3 = () => {
+  const name = 'State of a component'
+  const exercises = 14
+
+  return (
+    <>
+      <p>
+        Name: {name} | No. of exercises: {exercises}
+      </p>
+    </>
+  )
+}
+
 const Content = (props) => {
   return (
     <>
       <h2>Exercises:</h2>
-      <p>
-        Name: {props.part1} | # of Exercises: {props.exercises1}
-      </p>
-      <p>
-        Name: {props.part2} | # of Exercises: {props.exercises2}
-      </p>
-      <p>
-        Name: {props.part3} | # of Exercises: {props.exercises3}
-      </p>
+      <Part1 />
+      <Part2 />
+      <Part3 />
     </>
   )
 }
@@ -38,17 +71,14 @@ const Footer = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
   const exercises2 = 7
-  const part3 = 'State of a component'
   const exercises3 = 14
 
   return (
     <>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Content />
       <Footer exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </>
   )
