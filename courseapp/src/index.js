7eed93277da0d3ba80906863659a7cc79e424c10
Joe Collicoat <jcollicoat@gmail.com>
2019-08-22 19:51:52 +1200

    Migrate to mac

---
+++ courseapp/src/index.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Header
+
+const Header = (props) => {
+  return (
+    <>
+      <h3>Course:</h3>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+// Parts
+
+const Part1 = (props) => {
+  return (
+    <>
+      <p>
+        Name: <b>{props.part.name}</b> | No. of exercises: <b>{props.part.exercises}</b>
+      </p>
+    </>
+  )
+}
+
+const Part2 = (props) => {
+  return (
+    <>
+      <p>
+        Name: <b>{props.part.name}</b> | No. of exercises: <b>{props.part.exercises}</b>
+      </p>
+    </>
+  )
+}
+
+const Part3 = (props) => {
+
+  return (
+    <>
+      <p>
+        Name: <b>{props.part.name}</b> | No. of exercises: <b>{props.part.exercises}</b>
+      </p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <h2>Exercises:</h2>
+      <Part1 part={props.parts[0]} />
+      <Part2 part={props.parts[1]} />
+      <Part3 part={props.parts[2]} />
+    </>
+  )
+}
+
+// Total
+
+const Total = (props) => {
+  return (
+    <>
+      <h2>Total:</h2>
+      <p>Total number of exercises: <b>{props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</b></p>
+    </>
+  )
+}
+
+// App
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,100 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-// Header
+const Display = (props) => (
+  <div>{props.value}</div>
+)
 
-const Header = (props) => {
-  return (
-    <>
-      <h2>Course:</h2>
-      <h1>{props.course}</h1>
-    </>
-  )
-}
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
-// Parts
+const App = (props) => {
+  const [value, setValue] = useState(10)
 
-const Part1 = (props) => {
-  return (
-    <>
-      <p>
-        Name: <b>{props.part.name}</b> | No. of exercises: <b>{props.part.exercises}</b>
-      </p>
-    </>
-  )
-}
-
-const Part2 = (props) => {
-  return (
-    <>
-      <p>
-        Name: <b>{props.part.name}</b> | No. of exercises: <b>{props.part.exercises}</b>
-      </p>
-    </>
-  )
-}
-
-const Part3 = (props) => {
-
-  return (
-    <>
-      <p>
-        Name: <b>{props.part.name}</b> | No. of exercises: <b>{props.part.exercises}</b>
-      </p>
-    </>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <>
-      <h2>Exercises:</h2>
-      <Part1 part={props.parts[0]} />
-      <Part2 part={props.parts[1]} />
-      <Part3 part={props.parts[2]} />
-    </>
-  )
-}
-
-// Total
-
-const Total = (props) => {
-  return (
-    <>
-      <h2>Total:</h2>
-      <p>Total number of exercises: <b>{props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</b></p>
-    </>
-  )
-}
-
-// App
-
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const setToValue = (newValue) => {
+    setValue(newValue)
   }
 
   return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
+    <div>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="zero" />
+      <Button handleClick={() => setToValue(value + 1)} text="plus 1" />
+    </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
