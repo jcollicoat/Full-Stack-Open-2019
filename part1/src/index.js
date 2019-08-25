25e4aad09371d43ea6c88408c244cca2c1f78c74
Joe Collicoat <jcollicoat@gmail.com>
2019-08-25 18:27:31 +1200

    1.8 Commit
    unicafe step3

--- part1/src/index.js
+++ part1/src/index.js
@@ -7,8 +7,25 @@ const Button = (props) => (
   </button>
 )
 
-const App = (props) => {
-  // save clicks of each button to own state
+const Statistics = (props) => (
+  <div>
+    <h2>Statistics:</h2>
+    good: {props.good}
+    <br></br>
+    neutral: {props.neutral}
+    <br></br>
+    bad: {props.bad}
+    <br></br>
+    total: {props.good + props.neutral + props.bad}
+    <br></br>
+    average: {(props.good - props.bad) / (props.good + props.neutral + props.bad)}
+    <br></br>
+    positive: {(props.good) / (props.good + props.neutral + props.bad) * 100}%
+  </div>
+)
+
+const App = () => {
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -29,20 +46,7 @@ const App = (props) => {
       <Button handleClick={() => addToGood(good + 1)} text="good" />
       <Button handleClick={() => addToNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => addToBad(bad + 1)} text="bad" />
-      <h2>Statistics:</h2>
-      <div>
-        good: {good}
-        <br></br>
-        neutral: {neutral}
-        <br></br>
-        bad: {bad}
-        <br></br>
-        total: {good + neutral + bad}
-        <br></br>
-        average: {(good - bad) / (good + neutral + bad)}
-        <br></br>
-        positive: {good / (good + neutral + bad) * 100}%
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
