165c7bdc0281b08c11f45afdb7335f6e11b1ff89
Joe Collicoat <jcollicoat@gmail.com>
2019-09-03 17:36:55 +1200

    1.9 Commit
    unicafe step4

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,44 +1,52 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
-  </button>
-)
+const Button = props => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
 
-const Statistics = (props) => (
-  <div>
-    <h2>Statistics:</h2>
-    good: {props.good}
-    <br></br>
-    neutral: {props.neutral}
-    <br></br>
-    bad: {props.bad}
-    <br></br>
-    total: {props.good + props.neutral + props.bad}
-    <br></br>
-    average: {(props.good - props.bad) / (props.good + props.neutral + props.bad)}
-    <br></br>
-    positive: {(props.good) / (props.good + props.neutral + props.bad) * 100}%
-  </div>
-)
+const Statistics = props => {
+  if (props.total <= 0) {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        No statistics given
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        good: {props.good}
+        <br></br>
+        neutral: {props.neutral}
+        <br></br>
+        bad: {props.bad}
+        <br></br>
+        total: {props.total}
+        <br></br>
+        average: {(props.good - props.bad) / (props.total)}
+        <br></br>
+        positive: {(props.good / (props.total)) * 100}%
+      </div>
+    )
+  }
+};
 
 const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const addToGood = (newValue) => {
-    setGood(newValue)
-  }
-  const addToNeutral = (newValue) => {
-    setNeutral(newValue)
-  }
-  const addToBad = (newValue) => {
-    setBad(newValue)
-  }
+  const addToGood = newValue => {
+    setGood(newValue);
+  };
+  const addToNeutral = newValue => {
+    setNeutral(newValue);
+  };
+  const addToBad = newValue => {
+    setBad(newValue);
+  };
 
   return (
     <div>
@@ -46,11 +54,14 @@ const App = () => {
       <Button handleClick={() => addToGood(good + 1)} text="good" />
       <Button handleClick={() => addToNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => addToBad(bad + 1)} text="bad" />
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={good + neutral + bad}
+      />
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById("root"));
