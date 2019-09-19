4b7ed6b3bdafbaac532f3520638a88142a44f711
Joe Collicoat <jcollicoat@gmail.com>
2019-09-20 08:10:39 +1200

    2.5 Commit
    separate module

--- part2/courseapp/src/App.js
+++ part2/courseapp/src/App.js
@@ -1,31 +1,10 @@
 import React from "react";
-import Course from "./components/Course";
+import Courses from "./components/Courses";
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3
-      }
-    ]
-  };
-
   return (
     <div>
-      <Course course={course} />
+      <Courses />
     </div>
   );
 };
