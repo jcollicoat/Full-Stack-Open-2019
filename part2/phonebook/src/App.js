d7ccab74bc5a16a380a702895e84fe255a81d691
Joe Collicoat <jcollicoat@gmail.com>
2019-09-28 13:42:43 +1200

    2.11 Commit
    The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Phonebook from "./components/Phonebook";
 
-const App = props => {
-  const [persons, setPersons] = useState(props.persons);
+const App = () => {
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
   const [filterValue, setFilterValue] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then(response => {
+      setPersons(response.data);
+    });
+  });
 
   const handleFilterChange = event => {
     setFilterValue(event.target.value);
--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -3,17 +3,4 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
-const persons = [
-  {
-    name: "Arto Hellas",
-    phone: "040-1234567",
-    id: 1
-  },
-  {
-    name: "Joe",
-    phone: "123",
-    id: 2
-  }
-];
-
-ReactDOM.render(<App persons={persons} />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
